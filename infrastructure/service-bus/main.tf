b3d6aa79d02ecb33a6784c9fd867c813784e13bb
Stuart Shay <sshay@yahoo.com>
2025-09-02 20:22:19 +0000

    Update Versions

--- infrastructure/service-bus/main.tf
+++ infrastructure/service-bus/main.tf
@@ -6,7 +6,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 4.40"
+      version = "~> 4.42"
     }
     time = {
       source  = "hashicorp/time"
